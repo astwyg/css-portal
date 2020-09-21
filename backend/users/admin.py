8332d01100830f75741b72c5a07e2b80655c1e4a
WANG YG <astwyg@outlook.com>
2020-09-21 11:50:37 +0800

    update pdf file

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 
 from .models import Users
 
@@ -14,6 +15,17 @@ class UsersInline(admin.StackedInline):
 # Define a new User admin
 class UserAdmin(BaseUserAdmin):
     inlines = (UsersInline,)
+
+    actions = ['make_MSS']
+
+    def make_MSS(self, request, queryset):
+        group_MSS = Group.objects.get(name='MSS')
+        for user in queryset:
+            user.groups.add(group_MSS)
+            user.is_staff = True
+            user.save()
+        self.message_user(request, "{}个用户已被置为MSS权限".format(len(queryset)))
+    make_MSS.short_description = "设置为MSS职位(市场,销售,支持), 可以批量发邀请码"
 
 # Re-register UserAdmin
 admin.site.unregister(User)
