cf0596de3be87d8623db9f2e61947a27579d7fca
Hao Chen <chenhao@microduino.cc>
2020-11-02 12:05:22 +0800

    fix arm64 bug

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,3 @@
-docker build -t phytium/service:3-alpine3.12-x86 -f alpine.x86.Dockerfile .
+# docker build -t phytium/service:3-alpine3.12-x86 -f alpine.x86.Dockerfile .
 # docker build -t phytium/service:python3-ubuntu20-x86 -f ubuntu.x86.Dockerfile .
-# docker build -t phytium/service:python3-ubuntu20-arm64v8 -f ubuntu.arm64v8.Dockerfile .
+docker build -t phytium/service:python3-ubuntu20-arm64v8 -f ubuntu.arm64v8.Dockerfile .
